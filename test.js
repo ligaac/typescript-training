d6e4973a58eb817e5cb9a208ef4a558ac2afcc51
Norbert <kondora35@gmail.com>
2021-12-07 21:38:30 +0300

    un pic de ts

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var x;
 var y;
+<<<<<<< Updated upstream
 var z;
 z = 5;
 z = 'ceva';
@@ -73,3 +74,56 @@ function specialLog(num) {
     console.log(num + 5);
 }
 handler(1, 2, specialLog);
+=======
+var z; // de evitat, ce sens are ts daca folosesti asta ?
+z = 5;
+console.log(z);
+z = 'ceva';
+console.log(z);
+var car; // un fel de strctura ca in c
+car = {
+    brand: 'vw',
+    color: 'blue'
+};
+console.log(car.brand, car.color);
+;
+var a;
+var array = [1, 2, 3, 4, 5, 6, 7];
+console.log(array);
+array.push(256);
+console.log(array);
+// let array: string[], daca las asa e eroare trebuie intializat cu ceva gol daca nu pun nimic in el la inceput
+var cars = [];
+cars.push({ color: 'red', brand: 'fiat' });
+console.log(cars);
+array.forEach(function (elm) { return console.log(elm); });
+var vehicle_type;
+(function (vehicle_type) {
+    vehicle_type[vehicle_type["car"] = 0] = "car";
+    vehicle_type[vehicle_type["motorcycle"] = 1] = "motorcycle";
+    vehicle_type[vehicle_type["bike"] = 2] = "bike";
+    vehicle_type[vehicle_type["skateboard"] = 3] = "skateboard";
+})(vehicle_type || (vehicle_type = {}));
+;
+var mycar;
+var ceva; // poate lua ca valoare un string sau un numar
+var altceva; // ceva manarie
+var tof; // = let tof: true | false;
+function put(x) {
+    console.log(x);
+}
+;
+function add(x, y) {
+    return x + y;
+}
+;
+put(add(6, 7));
+function add5(x) {
+    return x + 5;
+}
+function salt(x, y, func) {
+    var result = x + y;
+    func(result);
+}
+salt(5, 6, add5);
+>>>>>>> Stashed changes
--- test.ts
+++ test.ts
@@ -1,93 +1,85 @@
 let x: number;
 let y: string;
-let z: any;
+let z: any;  // de evitat, ce sens are ts daca folosesti asta ?
+
 z = 5;
+console.log(z);
 z = 'ceva';
+console.log(z);
 
-enum PersonJob {
-    TEACHER,
-    ENGINEER,
-    POLICEMAN
-}
+let car: { brand: string, color: string}; // un fel de strctura ca in c
 
-interface IPerson {
-    name: string;
-    surname: string;
-    job: PersonJob;
-}
+car = {
+    brand: 'vw',
+    color: 'blue'
+};
+console.log(car.brand, car.color);
 
-/*let o: IPerson;
-
-o = {
-    name: 'nume',
-    surname: 'surname',
-    job: PersonJob.TEACHER
+interface car2 {   // asta seamana mai bine cu un struct din c
+    brand: string,
+    color: string
 };
 
-console.log(o.name, o.surname);
+let a: car2;
 
-let array: string[] = [];
+let array: number[] = [1, 2, 3, 4, 5, 6 ,7];
 
-array.push('ceva');
-array.push('ceva2');
-array.push('ceva3');
+console.log(array);
+array.push(256);
+console.log(array);
 
-array.forEach(element => console.log(element));
+// let array: string[], daca las asa e eroare trebuie intializat cu ceva gol daca nu pun nimic in el la inceput
 
-let people: IPerson[] = [];
+let cars: car2[] = [];
+cars.push({ color: 'red', brand: 'fiat'});
 
-people.push({ name: 'Rus', surname: 'Vlad', job: PersonJob.ENGINEER });
-people.push({ name: 'Valean', surname: 'Vlad', job: PersonJob.POLICEMAN });
+console.log(cars);
 
-people.forEach(element => console.log(element.name, element.surname, element.job));
+array.forEach(elm => console.log(elm));
 
-let a: number | string;
-a = 'a';
-a = 5;
-let b: string | 1;
-b = 'xulescu';
-//b = 2;
+enum vehicle_type {
+    car,
+    motorcycle,
+    bike,
+    skateboard
+};
 
-let v: string | null;*/
 
-function functie(): void {
-    console.log('altceva');
+let mycar: {brand: string, color: string, vehicleType: vehicle_type};
+
+let ceva: number | string; // poate lua ca valoare un string sau un numar
+let altceva: string | 1; // ceva manarie
+
+let tof: boolean; // = let tof: true | false;
+
+function put(x: number): void {
+    console.log(x);
+};
+
+function add(x: number, y: number): number {
+    return x + y;
+};
+
+put(add(6, 7));
+
+function add5(x: number): number{
+    return x + 5;
 }
 
-function add(a: number, b: number): number {
-    return a + b;
-}
-
-function comparePeople(p1: IPerson, p2: IPerson): boolean {
-    return (p1.name === p2.name) && (p1.surname === p2.surname) && (p1.job === p2.job);
-}
-
-let p1: IPerson = {
-    name: 'Andrei',
-    surname: 'Popescu',
-    job: PersonJob.TEACHER
-}
-
-let p2: IPerson = {
-    name: 'Andrei',
-    surname: 'Popescu',
-    job: PersonJob.TEACHER
-}
-
-console.log(comparePeople(p1, p2));
-
-console.log(p1 === p2);
-
-functie();
-console.log(add(4, 3));
-
-function handler(x: number, y: number, func: (z: number) => void): void {
-    let result = x + y;
+function salt(x: number, y: number, func: (z: number) => number): void{
+    let result: number = x + y;
     func(result);
 }
 
-function specialLog(num: number): void {
-    console.log(num + 5);
+console.log(salt(5, 6, add5));
+
+class mycity {
+    name: string;
+    population: number;
+    
+
+    hello(): string{
+        return 'Hello' + this.name;
+    }
 }
 
-handler(1, 2, specialLog);
